dae8c327885d3434fe2fcee0c6b3950b52e43871
ahmednule <nuleahmed6@gmail.com>
2024-07-28 22:23:43 +0300

    fixin on pycodestyle fails

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -30,7 +30,7 @@ def validUTF8(data):
 
         else:
             if not (i & mask_1 and not (i & mask_2)):
-                    return False
+                return False
 
         number_bytes -= 1
 
