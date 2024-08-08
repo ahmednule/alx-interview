2586e1d66988504d09fbef76bc0552eccc5c960b
ahmednule <nuleahmed6@gmail.com>
2024-08-09 01:44:52 +0300

    changed the aapi endpoint ebecause the other oe eas not working

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,16 +2,36 @@
 
 const request = require('request');
 
-request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (err, res, req) {
-  if (err) throw err;
-  const actors = JSON.parse(req).characters;
+request('https://swapi-api.alx-tools.com/api/people/' + process.argv[2], function (err, res, body) {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  
+  if (res.statusCode !== 200) {
+    console.error(`Failed to fetch data: ${res.statusCode}`);
+    return;
+  }
+
+  const actors = JSON.parse(body).characters;
   exactOrder(actors, 0);
 });
+
 const exactOrder = (actors, x) => {
   if (x === actors.length) return;
-  request(actors[x], function (err, res, req) {
-    if (err) throw err;
+
+  request(actors[x], function (err, res, body) {
+    if (err) {
+      console.error(err);
+      return;
+    }
+
+    if (res.statusCode !== 200) {
+      console.error(`Failed to fetch character: ${res.statusCode}`);
+      return;
+    }
+
     console.log(JSON.parse(body).name);
     exactOrder(actors, x + 1);
   });
-};
+}
