d79dfc0635838ef505c41bae156e45281899010a
ahmednule <nuleahmed6@gmail.com>
2024-08-09 02:30:17 +0300

    refratored my code to use async and await to tackle the failing checks

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,50 +1,37 @@
 #!/usr/bin/node
-
 const request = require('request');
 
-if (!process.argv[2]) {
-  console.error('Please provide a Movie ID as the first argument.');
-  process.exit(1);
-}
+const requestPromise = (url) => {
+  return new Promise((resolve, reject) => {
+    request(url, (err, res, body) => {
+      if (err) {
+        reject(err);
+      } else if (res.statusCode !== 200) {
+        reject(new Error(`HTTP ${res.statusCode}`));
+      } else {
+        resolve(body);
+      }
+    });
+  });
+};
 
-request(`https://swapi-api.alx-tools.com/api/films/${process.argv[2]}`, function (err, res, body) {
-  if (err) {
-    console.error('Error fetching movie:', err.message);
-    return;
-  }
-
-  if (res.statusCode !== 200) {
-    console.error(`Failed to fetch movie: HTTP ${res.statusCode}`);
-    return;
+const main = async () => {
+  if (!process.argv[2]) {
+    console.error('Please provide a Movie ID as the first argument.');
+    process.exit(1);
   }
 
   try {
-    const characters = JSON.parse(body).characters;
-    printCharactersInOrder(characters, 0);
-  } catch (parseError) {
-    console.error('Error parsing movie data:', parseError.message);
+    const filmBody = await requestPromise(`https://swapi-api.alx-tools.com/api/films/${process.argv[2]}`);
+    const characters = JSON.parse(filmBody).characters;
+
+    for (const characterUrl of characters) {
+      const characterBody = await requestPromise(characterUrl);
+      const character = JSON.parse(characterBody);
+      console.log(character.name);
+    }
+  } catch (error) {
+    console.error('Error:', error.message);
   }
-});
-
-const printCharactersInOrder = (characters, index) => {
-  if (index >= characters.length) return;
-
-  request(characters[index], function (err, res, body) {
-    if (err) {
-      console.error('Error fetching character:', err.message);
-      return;
-    }
-
-    if (res.statusCode !== 200) {
-      console.error(`Failed to fetch character: HTTP ${res.statusCode}`);
-      return;
-    }
-
-    try {
-      console.log(JSON.parse(body).name);
-      printCharactersInOrder(characters, index + 1);
-    } catch (parseError) {
-      console.error('Error parsing character data:', parseError.message);
-    }
-  });
 };
+main();
