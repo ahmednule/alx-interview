349d7c43d47ff757747de648d065e3a0946907c2
ahmednule <nuleahmed6@gmail.com>
2024-08-09 02:20:18 +0300

    fixed semistands fails

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,24 +2,21 @@
 
 const request = require('request');
 
-
 if (!process.argv[2]) {
   console.error('Please provide a Movie ID as the first argument.');
   process.exit(1);
 }
-
 
 request(`https://swapi-api.alx-tools.com/api/films/${process.argv[2]}`, function (err, res, body) {
   if (err) {
     console.error('Error fetching movie:', err.message);
     return;
   }
-  
+
   if (res.statusCode !== 200) {
     console.error(`Failed to fetch movie: HTTP ${res.statusCode}`);
     return;
   }
-
 
   try {
     const characters = JSON.parse(body).characters;
@@ -29,10 +26,8 @@ request(`https://swapi-api.alx-tools.com/api/films/${process.argv[2]}`, function
   }
 });
 
-
 const printCharactersInOrder = (characters, index) => {
   if (index >= characters.length) return;
-
 
   request(characters[index], function (err, res, body) {
     if (err) {
